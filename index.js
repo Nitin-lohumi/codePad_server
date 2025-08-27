bb948f9a402ad4c6ea209fd3e4b9c9a0057269cb
Nitin-Lohumi <lohuminitin@gmail.com>
2025-08-27 21:13:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,202 @@
+import express, { response } from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import axios from "axios";
+import dotenv from "dotenv"
+const app = express();
+const httpServer = http.createServer(app);
+const Io = new Server(httpServer, { cors: { origin: "*" } });
+dotenv.config();
+app.use(express.json());
+app.use(cors({
+    origin: "*",
+}))
+app.get("/", (req, res) => {
+    res.json({ msg: "hello" });
+});
+export const RoomNamespace = Io.of("/rooms");
+const roomCodes = {};
+const JoinedPeople = {};
+const CodeOutput = {};
+let admin;
+RoomNamespace.on("connection", (socket) => {
+    console.log("A user connected", socket.id);
+    socket.on("join-room", ({ RoomName, userName, isCreated }) => {
+        socket.roomName = RoomName;
+        socket.join(RoomName);
+        if (!JoinedPeople[RoomName]) {
+            admin = socket.id;
+            console.log(admin);
+            JoinedPeople[RoomName] = [];
+        }
+
+        const alreadyJoined = JoinedPeople[RoomName].some(
+            (user) => user.id === socket.id
+        );
+        if (!alreadyJoined) {
+            JoinedPeople[RoomName].push({ id: socket.id, userName });
+        }
+        if (roomCodes[RoomName]) {
+            socket.emit("init-code", roomCodes[RoomName]);
+        }
+        console.log(`${userName} (${socket.id}) joined room ${RoomName}, created=${isCreated}`);
+        console.log(JoinedPeople)
+        socket.to(RoomName).emit("userJoined", { id: socket.id, userName });
+        socket.to(RoomName).emit("getUsers", JoinedPeople[RoomName] || []);
+    });
+
+    socket.on("CheckValidate", ({ RoomName, iscreate }) => {
+        if (iscreate) {
+            if (JoinedPeople[RoomName]) {
+                socket.emit("check", false);
+            } else {
+                socket.emit("check", true);
+            }
+        } else {
+            if (!JoinedPeople[RoomName]) {
+                socket.emit("check", false);
+            } else {
+                socket.emit("check", true);
+
+            }
+        }
+        console.log(JoinedPeople)
+    })
+
+    socket.on("language", ({ lang, roomId, code }) => {
+        socket.to(roomId).emit("changelanguage", lang);
+        roomCodes[roomId] = code;
+        socket.broadcast.to(roomId).emit("code-change", roomCodes[roomId]);
+    });
+
+    socket.on("leave-room", ({ RoomName, userName }) => {
+        console.log(`${userName} left ${RoomName}`);
+        if (socket.id == admin) {
+            console.log("Admin  user is  disconnected", socket.id)
+            delete JoinedPeople[RoomName];
+        }
+        if (JoinedPeople[RoomName]) {
+            JoinedPeople[RoomName] = JoinedPeople[RoomName].filter(
+                (user) => user.id !== socket.id
+            );
+        }
+        for (let roomID in JoinedPeople) {
+            if (!JoinedPeople[roomID].length) delete JoinedPeople[roomID]
+        }
+        Io.to(RoomName).emit("getUsers", JoinedPeople[RoomName] || []);
+        socket.leave(RoomName);
+        socket.disconnect(true);
+    });
+
+    socket.on("code-change", ({ RoomName, code }) => {
+        roomCodes[RoomName] = code;
+        socket.to(RoomName).emit("code-change", code);
+    });
+
+    socket.on("newMsg", ({ username, msg, roomID }) => {
+        socket.broadcast.to(roomID).emit("getMsg", { username, msg, isMe: false });
+    });
+
+    socket.on("totalUsers", (RoomName) => {
+        console.log(JoinedPeople);
+        socket.emit("getUsers", JoinedPeople[RoomName] || []);
+    });
+
+    socket.on("isCodeRun", ({ socketId, RoomName }) => {
+        console.log("is code run", admin === socketId);
+        if (admin === socketId) {
+            console.log("is run tap by admin", true);
+            socket.to(RoomName).emit("runing", true);
+        }
+    })
+
+    socket.on("runLoading", ({ load, roomID }) => {
+        socket.to(roomID).emit("getloading", load);
+    })
+
+    socket.on("outputCode", ({ output, error, roomID }) => {
+        CodeOutput[roomID] = { output, error };
+        socket.to(roomID).emit("codeListen", CodeOutput[roomID]);
+    })
+
+    socket.on("disconnect", () => {
+        const RoomName = socket.roomName;
+        if (socket.id === admin) {
+            console.log("Admin  user is  disconnected", socket.id)
+            delete JoinedPeople[RoomName]
+        } else {
+            for (let roomID in JoinedPeople) {
+                JoinedPeople[roomID] = JoinedPeople[roomID].filter(
+                    (user) => user.id !== socket.id
+                );
+                Io.to(roomID).emit("getUsers", JoinedPeople[roomID]);
+            }
+            for (let roomID in JoinedPeople) {
+                if (!JoinedPeople[roomID].length) delete JoinedPeople[roomID]
+            }
+            console.log("A user disconnected", socket.id);
+        }
+        socket.disconnect(true);
+    });
+});
+
+app.post("/RunCode", async (req, res) => {
+    console.log("code is running");
+    const { code, languageId } = req.body;
+    if (!code || !languageId) {
+        return res.status(400).json({ error: "Code and languageId are required" });
+    }
+    console.log(code, languageId);
+    try {
+        const submissionResponse = await axios.post(
+            "https://judge0-ce.p.rapidapi.com/submissions",
+            {
+                source_code: Buffer.from(code).toString("base64"),
+                language_id: languageId
+            },
+            {
+                params: { base64_encoded: "true", wait: "false" },
+                headers: {
+                    "x-rapidapi-key": process.env.KEY,
+                    "x-rapidapi-host": process.env.HOST,
+                    "content-type": "application/json"
+                }
+            }
+        );
+
+        const token = submissionResponse.data.token;
+        if (!token) return res.status(500).json({ error: "Failed to get token" });
+
+        await new Promise((r) => setTimeout(r, 2000));
+
+        const resultResponse = await axios.get(
+            `https://judge0-ce.p.rapidapi.com/submissions/${token}`,
+            {
+                params: { base64_encoded: "true", fields: "*" },
+                headers: {
+                    "x-rapidapi-key": process.env.KEY,
+                    "x-rapidapi-host": process.env.HOST
+                }
+            }
+        );
+
+        const output = resultResponse.data.stdout
+            ? Buffer.from(resultResponse.data.stdout, "base64").toString("utf-8")
+            : "";
+        const error = resultResponse.data.stderr
+            ? Buffer.from(resultResponse.data.stderr, "base64").toString("utf-8")
+            : "";
+        console.log("output", output);
+        res.json({ output, error });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Something went wrong" });
+    }
+});
+
+
+console.log(roomCodes);
+httpServer.listen(4000, () => {
+    console.log("listen at port number -> http://localhost:4000");
+});
